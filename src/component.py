91a7a41e71b6acccfe0db31ed1741d8690e390e8
David Esner <david@keboola.com>
2022-11-18 16:24:15 +0100

    HOTFIX multi exception handling in main

--- src/component.py
+++ src/component.py
@@ -144,7 +144,7 @@ if __name__ == "__main__":
         comp = Component()
         # this triggers the run method by default and is controlled by the configuration.action parameter
         comp.execute_action()
-    except UserException | WriterUserException as exc:
+    except (UserException, WriterUserException) as exc:
         logging.exception(exc)
         exit(1)
     except Exception as exc:
